5a2ae7209610e87eaf488911c86aa0ceae91cdd0
brenno.lima <brenno.lima@soluti.com.br>
2021-12-08 13:56:32 -0300

    implemented log, exercise 101

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\{Usuario, Document};
+use Illuminate\Support\Facades\Log;
 
 class UsuarioController extends Controller
 {
     function index(){
         $usuario = Usuario::userInformation();
+        Log::info("Informações do usário carregado.");
         $document = Document::get();
+        Log::info("O documento carregado.");
         // $document = Document::all();
         return view('admin.usuario', compact('usuario', 'document'));
     }
